b6c2d7ba2251eed49b68b4ac0d654609d40cdeca
Muran94 <headiv94@gmail.com>
2017-10-17 20:05:23 +0900

    投票処理の実装とresultsビューレンプレートの作成。投票アプリケーションのメイン機能の実装完了

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,19 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse("質問%sの投票結果ページです" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("質問%sの投票結果ページです" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_question = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {'question': question, 'error_message': "選択されていません"}
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_question.votes += 1
+        selected_question.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
