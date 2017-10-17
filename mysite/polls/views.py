dc2113cf8172e00284e15b3d5121a771b92a0af6
Muran94 <headiv94@gmail.com>
2017-10-17 19:52:30 +0900

    detailビューテンプレート作成

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -10,7 +10,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("質問%sの詳細ページです" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("質問%sの投票結果ページです" % question_id)
