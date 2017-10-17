7428dc958fd46868016fd67675389f0bc49064f1
Muran94 <headiv94@gmail.com>
2017-10-17 19:44:52 +0900

    ビューの追加

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("こんにちは！")
+
+def detail(request, question_id):
+    return HttpResponse("質問%sの詳細ページです" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("質問%sの投票結果ページです" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("質問%sの投票結果ページです" % question_id)
