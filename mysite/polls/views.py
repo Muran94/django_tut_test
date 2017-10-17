0ceb020fdd9f95bcad971195019893789a644a5d
Muran94 <headiv94@gmail.com>
2017-10-17 19:49:34 +0900

    indexビューテンプレートの作成

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("こんにちは！")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("質問%sの詳細ページです" % question_id)
